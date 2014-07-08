bc6d1a128e7b5de596d9f84fc52163df6180f89b
Karthik RV <karthikrv82@gmail.com>
2014-07-08 12:19:11 +0530

    Final commit

--- plot3.R
+++ plot3.R
@@ -16,7 +16,7 @@ data$DateTime <- strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S")
 ## Creates the required plot and saves the plot to graphics device ( PNG in our case)
 ## The PNG file will be saved in the current working directory
 png(filename="plot3.png",width=480,height=480)
-with(data,plot(DateTime,Sub_metering_1,xlab="",ylab="Energy Sub Metering",type="n"))
+with(data,plot(DateTime,Sub_metering_1,xlab="",ylab="Energy sub metering",type="n"))
 with(data,points(DateTime,Sub_metering_1,type="l"))
 with(data,points(DateTime,Sub_metering_2,type="l",col="red"))
 with(data,points(DateTime,Sub_metering_3,type="l",col="blue"))
--- plot4.R
+++ plot4.R
@@ -20,19 +20,19 @@ par(mfrow=c(2,2))
 with(data, {
         plot(DateTime,Global_active_power,xlab="",ylab="Global Active Power",type='l')
         
-{
+        {
                 plot(DateTime,Voltage,xlab="datetime",ylab="Voltage",type="l",yaxt="n")
                 axis(2,at=seq(246,from=234,by=2),labels=FALSE)
                 axis(2,at=seq(246,from=234,by=4))
         }
 
-{
-        plot(DateTime,Sub_metering_1,xlab="",ylab="Energy Sub metering",type="n")
-        points(DateTime,Sub_metering_1,type="l")
-        points(DateTime,Sub_metering_2,type="l",col="red")
-        points(DateTime,Sub_metering_3,type="l",col="blue")
-        legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="n",cex=0.81)
-}
-plot(DateTime,Global_reactive_power,xlab="datetime",ylab="Global_reactive_power",type='l')
+        {
+                plot(DateTime,Sub_metering_1,xlab="",ylab="Energy sub metering",type="n")
+                points(DateTime,Sub_metering_1,type="l")
+                points(DateTime,Sub_metering_2,type="l",col="red")
+                points(DateTime,Sub_metering_3,type="l",col="blue")
+                legend("topright",lty=1,col=c("black","red","blue"),legend=c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),bty="n",cex=0.81)
+        }
+        plot(DateTime,Global_reactive_power,xlab="datetime",ylab="Global_reactive_power",type='l')
 })
 dev.off()
